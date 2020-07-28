2be2e0b4b5a505ab5d0b0b742b5a85823535d264
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-07-27 23:03:30 -0500

    Added input boxes on add time page

--- public/js/time.js
+++ public/js/time.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
-console.log(`yaya`)
+    console.log(`yaya`)
+    $(`.dropdown-item`).click(function(e){
+        let totalInputBoxes = $(this).text()
+        displayInputBox(totalInputBoxes);
+    })
+
+    function displayInputBox(totalInputBoxes){
+        $(`#input-boxes`).empty()
+        for (let i = 0; i <totalInputBoxes; i++){
+            const block = 
+            `<div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">First and last name</span>
+                    </div>
+                    <input type="text" aria-label="First name" class="form-control">
+                    <input type="text" aria-label="Last name" class="form-control">
+                </div> `;
+        
+            $(`#input-boxes`).append(block)
+        }
+    }
+
 
 })
