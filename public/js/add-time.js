7650eb883ebd0dc8dc0003db130f8c988f3a80ed
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-07-30 22:37:22 -0500

    added tables on add time page

--- public/js/add-time.js
+++ public/js/add-time.js
@@ -123,17 +123,48 @@ $(document).ready(function(){
 
          allSchedule = await getSchedule();
 
+        `<table class="table">
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+               
+            </tbody>
+        </table>`
+
         for (let i = 0; i<allSchedule.length; i++){
-            let time = allSchedule[i].totalDuration.map(function(time){
-               return `<p class="time">${time}</p>`
+            let time = allSchedule[i].totalDuration.map((time, index)=>{
+                const totalTime = time.split(`-`)
+               return `                
+               <tr>
+               <th scope="row">${index+1}</th>
+               <td>${totalTime[0]}</td>
+               <td>${totalTime[1]}</td>
+               </tr>`
             }).join(' ')
 
             let block= `
+            <h2>${allSchedule[i].totalTesters}</h2>
             <div class="schedule-block">
-                <h2>${allSchedule[i].totalTesters}</h2>
-                ${time}
-                <button id="${allSchedule[i]._id}" type="button" class="update-time-btn btn btn-success">Success</button>
+            <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">From</th>
+                <th scope="col">To</th>
+                </tr>
+            </thead>
+            <tbody>
+            ${time}
+            </tbody>
+            </table>
+            <div class="main-buttons">
+                <button id="${allSchedule[i]._id}" type="button" class="update-time-btn btn btn-success">Edit</button>
                 <button id="${allSchedule[i]._id}" type="button" class="delete-time-btn btn btn-danger">Delete</button>
+            </div>
             </div>`
 
             $(`.alltime-container`).append(block);
