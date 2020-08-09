87007437d16e1d56cf13699e791d86ed932e331d
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-08-09 15:31:46 -0500

    updated style on home page

--- public/js/index.js
+++ public/js/index.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
             const block = ` <div class="Tester-container ${currentClass}">
             <div class="tester-name" id="${availableTesters[i]._id}">${availableTesters[i].name}</div>
             <div class="tester-time">${availableTime.totalDuration[i]}</div>
-            <button id="${availableTesters[i]._id}" type="button" class="remove-tester-btn remove-btn btn btn-danger">Absent</button>
+            <button id="${availableTesters[i]._id}" type="button" class="remove-tester-btn remove-btn btn btn-danger">Unavailable</button>
             </div>
             `
             $(`.schedule-container`).append(block)
